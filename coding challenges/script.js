905911deff4f233b5fa0308ca641f6ef9318d438
Achrafcodes <essoussygamer@gmail.com>
2022-09-29 14:59:54 +0100

    set

--- coding challenges/script.js
+++ coding challenges/script.js
@@ -15,15 +15,18 @@ GOOD LUCK 😀
 */
 
 const gameEvents = new Map([
-    [17, "⚽️ GOAL"],
-    [36, "🔁 Substitution"],
-    [47, "⚽️ GOAL"],
-    [61, "🔁 Substitution"],
-    [64, "🔶 Yellow card"],
-    [69, "🔴 Red card"],
-    [70, "🔁 Substitution"],
-    [72, "🔁 Substitution"],
-    [76, "⚽️ GOAL"],
-    [80, "⚽️ GOAL"],
-    [92, "🔶 Yellow card"],
-])
+        [17, "⚽️ GOAL"],
+        [36, "🔁 Substitution"],
+        [47, "⚽️ GOAL"],
+        [61, "🔁 Substitution"],
+        [64, "🔶 Yellow card"],
+        [69, "🔴 Red card"],
+        [70, "🔁 Substitution"],
+        [72, "🔁 Substitution"],
+        [76, "⚽️ GOAL"],
+        [80, "⚽️ GOAL"],
+        [92, "🔶 Yellow card"],
+    ])
+    // 1. Create an array 'events' of the different game events that happened (no duplicates)
+let events = new Set(gameEvents.keys())
+console.log(events);
