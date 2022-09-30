6502e4d75eee47aff4d6ba81ccf7b6791982e99a
Achrafcodes <essoussygamer@gmail.com>
2022-09-30 14:22:41 +0100

    save

--- coding challenges/script.js
+++ coding challenges/script.js
@@ -6,10 +6,9 @@
 Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
 1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-      [FIRST HALF] 17: ⚽️ GOAL
+[FIRST HALF] 17: ⚽️ GOAL
 
 GOOD LUCK 😀
 */
@@ -28,8 +27,12 @@ const gameEvents = new Map([
         [92, "🔶 Yellow card"],
     ])
     // 1. Create an array 'events' of the different game events that happened (no duplicates)
-let events = new Set(gameEvents.entries())
-console.log(events);
-gameEvents.delete(64)
-console.log(events)
-console.log(events.delete(64))
+const events = new Set(gameEventsnt)
+    //2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+let yellowCard = gameEvents.delete(64)
+    // console.log(gameEvents.delete(64))
+console.log(gameEvents)
+console.log(yellowCard);
+
+
+//3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
