2029e827bfac0826b83ce919debddf71673d27d9
achraf essoussy <88954138+Achrafcodes@users.noreply.github.com>
2022-10-10 00:50:34 +0100

    challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -3,7 +3,12 @@ document.body.append(document.createElement('button'));
 document.querySelector("button").addEventListener("click", function() {
 
     let key = document.querySelector("textarea").value;
-    let splited = key.split("\n")
-    console.log(splited);
+    let rows = key.split("\n")
+    for (const [i, row] of rows.entries()) {
 
+        let [first, second] = row.toLowerCase().trim().split("_");
+        let log = `${first}${second.replace(second[0],second[0].toUpperCase())+second.slice() }`
+        console.log(`${log.padEnd(20)}${'🤞'.repeat(i+1)}`)
+
+    }
 })
