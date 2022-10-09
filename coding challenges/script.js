2f2e000c2b682ae3495fa4603ce6c815963b3c25
achraf essoussy <88954138+Achrafcodes@users.noreply.github.com>
2022-10-09 16:10:01 +0100

    test 1

--- coding challenges/script.js
+++ coding challenges/script.js
@@ -12,38 +12,3 @@ Let's continue with our football betting app! This time, we have a map with a lo
 
 GOOD LUCK 😀
 */
-
-const gameEvents = new Map([
-    [17, "⚽️ GOAL"],
-    [36, "🔁 Substitution"],
-    [47, "⚽️ GOAL"],
-    [61, "🔁 Substitution"],
-    [64, "🔶 Yellow card"],
-    [69, "🔴 Red card"],
-    [70, "🔁 Substitution"],
-    [72, "🔁 Substitution"],
-    [76, "⚽️ GOAL"],
-    [80, "⚽️ GOAL"],
-    [92, "🔶 Yellow card"],
-]);
-// 1. Create an array 'events' of the different game events that happened (no duplicates)
-const events = [new Set(gameEvents.values())]
-console.log(events)
-    //2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-gameEvents.delete(64);
-// console.log(gameEvents.delete(64))
-
-console.log(gameEvents);
-
-//3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-let time = [...gameEvents.keys()].pop()
-console.log(time)
-let avv = `an event in the game is every ${time/gameEvents.size} minutes`
-
-console.log(avv)
-    // 4. Loop over the events and log them to the console, marking whether it 's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽️GOAL
-for (const [min, event] of gameEvents.entries()) {
-    const half = min <= 45 ? 'First' : 'Second';
-    console.log(`${half} half ${min}:${event}`);
-
-}
