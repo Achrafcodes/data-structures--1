1be60fb77f839ebcc19060adb0c7a81841fd40f0
achraf essoussy <88954138+Achrafcodes@users.noreply.github.com>
2022-10-10 00:20:09 +0100

    hello

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,33 +1,6 @@
-"use strict"
-// Data needed for a later exercise
-const openingHours = {
-    thu: {
-        open: 12,
-        close: 22,
-    },
-    fri: {
-        open: 11,
-        close: 23,
-    },
-    sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-    },
-}
-const flights =
-    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30"
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector("button").addEventListener("click", function() {
 
-array()
-    // Data needed for first part of the section
-const restaurant = {
-        data: 'hello',
-        name: "Classico Italiano",
-        location: "Via Angelo Tavanti 23, Firenze, Italy",
-        categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-        starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-        mainMenu: ["Pizza", "Pasta", "Risotto"],
-
-        openingHours,
-    }
-    //create a function thats take the name and make it capitalized 
-let newName = [];
+    let key = document.querySelector("textarea").value;
+})
